b04b99054e9bc4bb1aab8232974ba6e69a96bca9
Julia Grab <julia.grab@ucalgary.ca>
2019-11-20 17:02:06 -0700

    Update

---
+++ src/RentalPropertyManagementSystem/GUI/ChangeListingScreen.java
@@ -0,0 +1,10 @@
+package RentalPropertyManagementSystem.GUI;
+
+import javax.swing.*;
+
+public class ChangeListingScreen {
+    private JPanel panel;
+    private JButton changeStateButton;
+    private JComboBox comboBox1;
+    private JButton cancelButton;
+}
